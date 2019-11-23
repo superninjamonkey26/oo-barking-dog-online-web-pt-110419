4320df2cce79b7a453379cde9551974cdca2a50c
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-11-23 21:34:02 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,7 @@
 class Dog
-  #def dog_name
-   # dog_name = "Fido"
-#  end
+  def name = (dog_name)
+    @dog_name = "Fido"
+  end
 end
-#fido = Dog.new
-#fido.name = "Fido"
+fido = Dog.new
+fido.name = "Fido"
